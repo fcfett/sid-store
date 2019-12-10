338b3e7ccb15a39911502be32121ca8379a3d5d3
Felipe Fett <felipe.fett@opngo.com.br>
2019-12-10 08:51:03 -0300

    Updates

---
+++ precache-manifest.6d7cb66513623c5b2cba480b312000d4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a144bdf9d6c6c4288029891fd82efa69",
+    "url": "/sid-store/index.html"
+  },
+  {
+    "revision": "b90fd92dc16e73c19cfc",
+    "url": "/sid-store/static/css/main.10d22cf7.chunk.css"
+  },
+  {
+    "revision": "02345bc63ea6c113637d",
+    "url": "/sid-store/static/js/2.ea0a22ab.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/sid-store/static/js/2.ea0a22ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b90fd92dc16e73c19cfc",
+    "url": "/sid-store/static/js/main.3940a71b.chunk.js"
+  },
+  {
+    "revision": "866182bbe5db38d5965c",
+    "url": "/sid-store/static/js/runtime-main.080a4913.js"
+  },
+  {
+    "revision": "56c8ef7f5333f94ffdd7a0e5ddcb4475",
+    "url": "/sid-store/static/media/angle-down.56c8ef7f.svg"
+  },
+  {
+    "revision": "bda910fcb927e7ba64e10d3c9be51a53",
+    "url": "/sid-store/static/media/logo.bda910fc.svg"
+  },
+  {
+    "revision": "551d595ec4ea0d1c41a0fbe495ec85e6",
+    "url": "/sid-store/static/media/magnify.551d595e.svg"
+  },
+  {
+    "revision": "a1148ea0ec5e18cecfb9ac8aa6944d06",
+    "url": "/sid-store/static/media/plus.a1148ea0.svg"
+  }
+]);
